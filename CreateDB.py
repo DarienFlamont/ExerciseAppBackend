73b58ae96b62f49398c2f66f0375af219fd1e5eb
dflamont <dflamont@sfu.ca>
2019-12-18 16:58:23 -0800

    Adding collections to DB now saved after pulling gitignore

--- CreateDB.py
+++ CreateDB.py
@@ -1,6 +1,12 @@
 import pymongo
 
+#Creating the client from our Atlas MongoDB
 client = pymongo.MongoClient("mongodb+srv://Darien:test123@fitnesssystem-wtaxp.mongodb.net/test?retryWrites=true&w=majority")
 db = client.test
 
-print(client.list_database_names())
+workouts_collection = db['workouts']
+users_collection = db['users']
+exercise_hierarchy_collection = db['exercise_hierarchy']
+
+#prints all the collections in the DB
+print(db.list_collection_names())

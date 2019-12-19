84616d294da9347b338eb62dad58c8726c04de32
dflamont <dflamont@sfu.ca>
2019-12-18 17:37:46 -0800

    creating a bash script to install dependencies in windows 10

---
+++ install_dependencies.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#On Windows 10 navigate to the directory of the script and run .\install_dependecies.sh (Make sure you have pip installed on your machine)
+
+bash -c "python -m pip install pymongo, dnspython, graphene, Flask"

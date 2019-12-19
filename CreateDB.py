e68a3be01259d8bec7fd4a69917d858c783604e7
dflamont <dflamont@sfu.ca>
2019-12-18 17:11:27 -0800

    Created our collections on Atlas dbMongo and can access them in python, also has an insert for a user

--- CreateDB.py
+++ CreateDB.py
@@ -2,6 +2,8 @@ import pymongo
 
 #Creating the client from our Atlas MongoDB
 client = pymongo.MongoClient("mongodb+srv://Darien:test123@fitnesssystem-wtaxp.mongodb.net/test?retryWrites=true&w=majority")
+
+#test is the database on the cluster that we are inserting collections into (can also be done on Atlas interface)
 db = client.test
 
 workouts_collection = db['workouts']
@@ -10,3 +12,11 @@ exercise_hierarchy_collection = db['exercise_hierarchy']
 
 #prints all the collections in the DB
 print(db.list_collection_names())
+
+#Try inserting a user
+#user = {'name': 'Darien', 'weight': '160lbs', 'height': '6\'0'}
+
+#Insert_one returns a object that has a property inserted_id that is some kind of hash (running this script will insert multiple Darien's)
+#x = users_collection.insert_one(user)
+#print(x.inserted_id)
+

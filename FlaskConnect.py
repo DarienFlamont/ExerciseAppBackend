f65876d979a464f0fd3ff3737a6caeee41995efe
dflamont <dflamont@sfu.ca>
2019-12-18 22:58:43 -0800

    ran CreateDB script so there is data in the database.  Also tried creating the FlaskConnect but not sure about routing and actually running the webserver for GET and POST examples

---
+++ FlaskConnect.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask import jsonify
+from flask import request
+from flask_pymongo import PyMongo
+
+app = Flask(__name__)
+
+app.config['MONGO_DBNAME'] = 'test'
+app.config['MONGO_URI'] = 'mongodb+srv://Darien:test123@fitnesssystem-wtaxp.mongodb.net/test?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
+
+@app.route('/user', methods=['GET'])
+def get_all_users():
+	user = mongo.db.test
+	output = []
+	for u in user.find():
+		output.append({'name' : u['name'], 'weight' : u['weight'], 'height' : u['height']})
+	return jsonify({'result': output})
+
+if __name__ == '__main__':
+    app.run(debug=True)

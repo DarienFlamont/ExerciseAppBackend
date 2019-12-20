5a2060544606f0234e5f11f13a26a9841aeb67f5
aPruner <adampruner99@gmail.com>
2019-12-20 15:56:39 -0800

    start buiding out auth API, signup method works with plaintext passwords and very little error handling

--- auth.py
+++ auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
 from . import mongo
 
 auth = Blueprint("auth", __name__)
@@ -9,10 +9,31 @@ def login():
     return 'Login'
 
 
-@auth.route('/signup')
-def signup():
-    return 'Signup'
+@auth.route('/signup', methods=['POST'])
+def signup_post():
+    data = request.get_json()
+    email = data.get('email')
+    first_name = data.get('first_name')
+    last_name = data.get('last_name')
+    password = data.get('password')
 
+    email_exists = mongo.db.users.count_documents({'email': email}) > 0
+
+    # TODO: Properly handle the case where email already exists in the database
+    #  (further error handling)
+    if email_exists:
+        return 'User already exists'
+
+    # Create a user and write it to the db
+    # TODO: Generate password hash
+    # TODO: Create MongoDB user model - Darien is doing this
+    mongo.db.users.insert_one({
+        'email': email,
+        'first_name': first_name,
+        'last_name': last_name,
+        'password': password
+    })
+    return 'Signup successful'
 
 @auth.route('/logout')
 def logout():

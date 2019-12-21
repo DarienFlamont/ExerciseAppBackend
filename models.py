525c675b8082bded6c02185bf12477811ee8a285
dflamont <dflamont@sfu.ca>
2019-12-21 02:22:51 -0800

    Fixed typo errors and added new lines

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 from pymodm import MongoModel, EmbeddedMongoModel, fields
 
+
 class User(MongoModel):
 	user_id = fields.IntegerField(min_value=0, primary_key=True)
 	first_name = fields.CharField(max_length=50)
@@ -9,16 +10,18 @@ class User(MongoModel):
 	height_inches = fields.IntegerField(min_value=0, max_value=120)
 	weight_lbs = fields.IntegerField(min_value=0, max_value=600)
 
+
 class Workout(MongoModel):
 	date = fields.DateTimeField()
-	total_gym_time = fields.IntgerField(min_value=0, max_value=600)
-	total_rest_time = fields.IntgerField(min_value=0, max_value=600)
+	total_gym_time = fields.IntegerField(min_value=0, max_value=600)
+	total_rest_time = fields.IntegerField(min_value=0, max_value=600)
 	exercises_completed = fields.EmbeddedDocumentListField('CompletedExercise')
+
 
 class CompletedExercise(EmbeddedMongoModel):
 	exercise_name = fields.CharField(max_length=50, primary_key=True)
 	exercise_classification = fields.CharField(max_length=50)
-	reps = fields.IntgerField(min_value=0, max_value=20)
-	sets = fields.IntgerField(min_value=0, max_value=10)
-	weight = fields.IntgerField(min_value=0, max_value=1000)
-	time = fields.IntgerField(min_value=0, max_value=600)
+	reps = fields.IntegerField(min_value=0, max_value=20)
+	sets = fields.IntegerField(min_value=0, max_value=10)
+	weight = fields.IntegerField(min_value=0, max_value=1000)
+	time = fields.IntegerField(min_value=0, max_value=600)

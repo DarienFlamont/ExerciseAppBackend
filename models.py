e981a8355772abfa75b5e9ae44fd10caa86548fe
dflamont <dflamont@sfu.ca>
2019-12-21 01:08:37 -0800

    Fixed naming issues and added a user_id field in the User class

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 from pymodm import MongoModel, fields
 
 class User(MongoModel):
+	user_id = fields.IntegerField(min_value = 0)
 	first_name = fields.CharField(max_length = 50)
 	last_name = fields.CharField(max_length = 50)
 	email = fields.EmailField(primary_key = True)
@@ -12,11 +13,11 @@ class Workout(MongoModel):
 	date = fields.DateTimeField()
 	total_gym_time = fields.IntgerField(min_value = 0, max_value = 600)
 	total_rest_time = fields.IntgerField(min_value = 0, max_value = 600)
-	exercises = fields.EmbeddedDocumentListField('Exercise')
+	exercises_completed = fields.EmbeddedDocumentListField('CompletedExercise')
 
-class Exercise(EmbeddedMongoModel):
-	exercise_name = fields.CharField(max_lenght = 50, primary_key = True)
-	exercise_classification = fields.CharField(max_lenght = 50)
+class CompletedExercise(EmbeddedMongoModel):
+	exercise_name = fields.CharField(max_length = 50, primary_key = True)
+	exercise_classification = fields.CharField(max_length = 50)
 	reps = fields.IntgerField(min_value = 0, max_value = 20)
 	sets = fields.IntgerField(min_value = 0, max_value = 10)
 	weight = fields.IntgerField(min_value = 0, max_value = 1000)

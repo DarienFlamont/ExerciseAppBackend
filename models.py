c5b19beefd996906414d640538c3147d181190e1
dflamont <dflamont@sfu.ca>
2019-12-21 02:11:48 -0800

    Changed primary key to user_id and made the email field empty

--- models.py
+++ models.py
@@ -1,10 +1,10 @@
 from pymodm import MongoModel, fields
 
 class User(MongoModel):
-	user_id = fields.IntegerField(min_value=0)
+	user_id = fields.IntegerField(min_value=0, primary_key=True)
 	first_name = fields.CharField(max_length=50)
 	last_name = fields.CharField(max_length=50)
-	email = fields.EmailField(primary_key=True)
+	email = fields.EmailField()
 	hashed_password = fields.CharField(max_length=256)
 	height_inches = fields.IntegerField(min_value=0, max_value=120)
 	weight_lbs = fields.IntegerField(min_value=0, max_value=600)

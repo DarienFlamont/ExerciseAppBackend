55f7b6cae2788abedbd584fdb0d386acd6a8668f
dflamont <dflamont@sfu.ca>
2019-12-20 23:17:43 -0800

    Created more models and submodels for exercies (can decide if we want to include muscle groups). Should be able to include a workout with an exercise after figuring out the exercise heirarchy

--- models.py
+++ models.py
@@ -7,3 +7,16 @@ class User(MongoModel):
 	hashed_password = fields.CharField(max_length = 256)
 	height_inches = fields.IntegerField(min_value = 0, max_value = 120)
 	weight_lbs = fields.IntegerField(min_value = 0, max_value = 600)
+
+class Workout(MongoModel):
+	date = fields.DateTimeField()
+	total_gym_time = fields.IntgerField(min_value = 0, max_value = 600)
+	total_rest_time = fields.IntgerField(min_value = 0, max_value = 600)
+	exercises = fields.EmbeddedDocumentListField('Exercise')
+
+class Exercise(EmbeddedMongoModel):
+	exercise_name = fields.CharField(max_lenght = 50)
+	reps = fields.IntgerField(min_value = 0, max_value = 20)
+	sets = fields.IntgerField(min_value = 0, max_value = 10)
+	weight = fields.IntgerField(min_value = 0, max_value = 1000)
+	time = fields.IntgerField(min_value = 0, max_value = 600)

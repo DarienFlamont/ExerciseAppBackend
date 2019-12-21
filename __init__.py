92572477119add510f5ceaa0d08482cacc4ac2a6
aPruner <adampruner99@gmail.com>
2019-12-21 01:14:12 -0800

    start working on login_manager for persistent login

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_pymongo import PyMongo
+from flask_login import LoginManager
 
 mongo = PyMongo()
 
@@ -11,6 +12,16 @@ def create_app():
 	app.config['MONGO_URI'] = 'mongodb+srv://Darien:test123@fitnesssystem-wtaxp.mongodb.net/test?retryWrites=true&w=majority'
 
 	mongo.init_app(app)
+
+	login_manager = LoginManager()
+	login_manager.login_view = 'auth.login'
+	login_manager.init_app(app)
+
+	from .models import User
+
+	@login_manager.user_loader
+	def load_user(user_id):
+		return mongo.db.users.find({'user_id': user_id})
 
 	from .auth import auth as auth_blueprint
 	app.register_blueprint(auth_blueprint)
